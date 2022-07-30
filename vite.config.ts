53df96346b3502056999237b93e572ef8648c6cc
Tanimodori <unknowner2014@gmail.com>
2022-07-30 22:23:54 +0800

    build: Skip builtins overheads

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import path from 'path';
 import glob from 'glob';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      builtins: path.resolve(__dirname, 'generator/app/builtins'),
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
@@ -13,7 +18,7 @@ export default defineConfig({
       formats: ['cjs'],
     },
     rollupOptions: {
-      external: ['yeoman-generator', 'fs'],
+      external: ['yeoman-generator', 'yo', 'fs'],
       input: glob.sync(path.resolve(__dirname, 'generator/**/*.ts')),
       output: {
         preserveModules: true,
