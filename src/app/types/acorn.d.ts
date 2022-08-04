f0e2271b713088199badfcb53e522a458b68bf44
Tanimodori <unknowner2014@gmail.com>
2022-08-04 15:00:53 +0800

    types: Fix ExtendNode type

--- src/app/types/acorn.d.ts
+++ src/app/types/acorn.d.ts
@@ -1,26 +1,16 @@
-import type { Options } from 'acorn';
 import * as ESTree from 'estree';
 
 declare module 'acorn' {
-  type DistributiveExtendNode<T> = T extends Record<string, unknown>
-    ? ExtendNode<T>
-    : T extends Array<infer E>
-    ? Array<E> extends T
-      ? Array<ExtendNode<E>>
-      : T
-    : T;
+  type ExtendObject<T> = {
+    [K in keyof T]: ExtendNode<T[K]>;
+  };
   type WithStartEnd<T> = T extends ESTree.Node | ESTree.Comment
-    ? {
-        start: number;
-        end: number;
-      }
+    ? { start: number; end: number }
     : unknown;
-  export type ExtendNode<T> = {
-    [K in keyof T]: DistributiveExtendNode<T[K]>;
-  } & WithStartEnd<T>;
-
+  export type ExtendNode<T> = T extends object ? ExtendObject<T> & WithStartEnd<T> : T;
   export function parse(s: string, o: Options): ExtendNode<ESTree.Program>;
 
+  // fix type of Comment property 'type'
   export type AcornComment = Omit<Comment, 'type'> & {
     type: 'Line' | 'Block';
   };
