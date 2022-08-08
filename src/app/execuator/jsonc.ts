8bc7653ce4cb6df66bb485cae0b51879422be375
Tanimodori <unknowner2014@gmail.com>
2022-08-08 15:45:07 +0800

    feat: format jsonc

--- src/app/execuator/jsonc.ts
+++ src/app/execuator/jsonc.ts
@@ -1,6 +1,12 @@
 import { applyEdits, modify, parseTree } from 'jsonc-parser';
 
 export const editJsonc = (...args: Parameters<typeof modify>) => {
+  args[3] = {
+    formattingOptions: {
+      tabSize: 2,
+      insertSpaces: true,
+    },
+  };
   const edits = modify(...args);
   return applyEdits(args[0], edits);
 };
